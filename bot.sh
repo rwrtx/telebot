38270d7efc18ce603b4d39ef5411c1cc36c4ee79
rwrtx <152061298+rwrtx@users.noreply.github.com>
2025-01-04 09:38:01 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -6,7 +6,7 @@ rm -rf cybervpn
 apt update && apt upgrade -y
 apt install python3 python3-pip -y
 apt install sqlite3 -y
-wget https://raw.githubusercontent.com/rwrtx/telebot/main/bot/cybervpn.zip
+wget https://raw.githubusercontent.com/rwrtx/telebot/main/cybervpn.zip
 unzip cybervpn.zip
 cd cybervpn
 rm var.txt
